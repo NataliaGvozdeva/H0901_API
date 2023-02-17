356c0041563f84310675379ef7f173a481570fba
Natalia Gvozdeva (intern) <gvozdeva-natalia@mail.ru>
2023-02-18 00:50:11 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+
+    public FormDate birthday; // поле для дат рождения
+    public String name; // поле для имени
+    public String passport; // поле для номера паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для обозначение подписки
+
+}
